60414c531342dfc43153d5a0de9147d98aa4c698
gabriele <gabrielescodinu96@gmail.com>
2020-12-10 15:00:22 +0100

    commit 02

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,50 +1,45 @@
 $(document).ready(function () {
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
+  const lista = [
+    {
+      name: "cat",
+      family: "animal",
+      icon: "fas fa-cat",
+    },
+    {
+      name: "dog",
+      family: "animal",
+      icon: "fas fa-dog",
+    },
+    {
+      name: "car",
+      family: "vehicle",
+      icon: "fas fa-car",
+    },
+    {
+      name: "tractor",
+      family: "vehicle",
+      icon: "fas fa-tractor",
+    },
+  ];
 
+  // Selezioniamo il container icons
+  const icons = document.getElementById('icons');
+
+  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+  lista.forEach(listaElemento => {
+    const markup = `
+    <div>
+      <i class="${lista.icon}"></i>
+      ${lista.name}
+    </div>
+    `;
+    icons.insertAdjacentHTML('beforeend', markup);
+  });
 });
-// Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
-const lista = [
-  {
-    name: "cat",
-    family: "animal",
-    icon: "fas fa-cat",
-    color: "blue",
-  },
-  {
-    name: "dog",
-    family: "animal",
-    icon: "fas fa-dog",
-    color: "blue",
-  },
-  {
-    name: "car",
-    family: "vehicle",
-    icon: "fas fa-car",
-    color: "green",
-  },
-  {
-    name: "tractor",
-    family: "vehicle",
-    icon: "fas fa-tractor",
-    color: "green",
-  },
-];
-
-// Selezioniamo il container icons
-const icons = document.getElementById('icons');
-
-//inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
 
 
 /* ---- FUNCTIONS ----*/
 // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
-lista.forEach(listaElemento => {
-  const markup = `
-  <div>
-    <i class="${lista.icon}" style="color:${lista.color}"></i>
-    ${lista.name}
-  </div>
-  `;
-  icons.insertAdjacentHTML('beforeend', markup);
-});
 // tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
 // tip: il template literal ci puo aiutare con il markup
--- milestone2/icons.js
+++ milestone2/icons.js
@@ -1,24 +1,63 @@
 $(document).ready(function () {
   // Qui le icone che abbiamo definito nella milestone 1
+  const lista = [
+    {
+      name: "cat",
+      family: "animal",
+      icon: "fas fa-cat",
+    },
+    {
+      name: "dog",
+      family: "animal",
+      icon: "fas fa-dog",
+    },
+    {
+      name: "car",
+      family: "vehicle",
+      icon: "fas fa-car",
+    },
+    {
+      name: "tractor",
+      family: "vehicle",
+      icon: "fas fa-tractor",
+    },
+  ];
 
   // definiamo dei colori per le icone (blue, orange, purple)
-
+  const blue;
+  const orange;
+  const purple;
 
   //aggiungiamo dei colori usando una funzione
+  const nuovaLista = cats.map(cat => {
+    const blue = lista.colorBlue;
+    const orange = lista.colorOrange;
+    const purple = lista.colorPurple;
 
+    return {
+      name,
+      family,
+      icon,
+    }
+  });
 
   //inseriamo le icone colorate nel container
+  nuovaLista.forEach(nuovaListaElemento => {
+    const markup = `
+    <div>
+    <i class="${lista.icon}" style="color:${nuovaLista.color}"></i>
+    ${nuovaLista.name}
+    </div>
+    `;
+    icons.insertAdjacentHTML('beforeend', markup);
+  });
 
 });
 
 
 /* ---- FUNCTIONS ----*/
 //1. Funzione milestone 1
-
-
 //2. funzione per assegnare un colore ad un icona
-// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map 
-
-
+// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map
 //3. funzione per ottenere da un array
 // tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
